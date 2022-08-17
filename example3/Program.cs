7b0eda3d6e7dfa630f6a674eba378c0509004dc4
Olga <olyakapran@yandex.by>
2022-08-17 14:24:42 +0300

    Sum

---
+++ example3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace example_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 31;
+            int numberB = 5;
+            int result = numberB + numberA ;
+            Console.WriteLine(result);
+
+        }
+    }
+}
